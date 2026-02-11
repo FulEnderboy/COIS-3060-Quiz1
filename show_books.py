6901b5ceaa911395f8a9d8d06dbb2d60eec56a30
Danny Kleiner Perez Luna <enderboygamer18@gmail.com>
2026-02-11 10:28:11 -0500

    Show no books if catalog is empty - fixes #1

--- show_books.py
+++ show_books.py
@@ -2,5 +2,7 @@ print('Library Catalog')
 print('=' * 50)
 
 with open('books.txt', 'r') as f:
+    if f.readline() == "":
+        print("The catalog is empty. Add some books!")
     for line in f:
         print(line.strip())
